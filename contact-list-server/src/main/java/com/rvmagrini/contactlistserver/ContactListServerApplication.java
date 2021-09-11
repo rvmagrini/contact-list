58a84cbaf5e3905c58c948b06152f4d2440334ac
Renato Magrini <renatomagrini@gmail.com>
2021-09-11 10:58:07 +0200

    [1.1] Created Spring Boot project

---
+++ contact-list-server/src/main/java/com/rvmagrini/contactlistserver/ContactListServerApplication.java
@@ -0,0 +1,13 @@
+package com.rvmagrini.contactlistserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactListServerApplication.class, args);
+	}
+
+}
---
+++ contact-list-server/src/test/java/com/rvmagrini/contactlistserver/ContactListServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvmagrini.contactlistserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
