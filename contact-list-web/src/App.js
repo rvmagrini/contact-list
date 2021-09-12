2b6d0cdf57a5f2f589bed82789a0ce7c77e118dc
Renato Magrini <renatomagrini@gmail.com>
2021-09-12 13:04:11 +0200

    [1.9] Configured routing  in react application

--- contact-list-web/src/App.js
+++ contact-list-web/src/App.js
@@ -2,15 +2,24 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ListContactComponent from './components/ListContactComponent';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
       <div>
-        <Header />
-        <div className="container">
-            <ListContactComponent />
-        </div>
-        <Footer />
+        <Router>
+            <Header />
+
+            <div className="container">
+              <Switch> http://localhost:3000/contacts
+                <Route path = "/" component = {ListContactComponent}></Route>
+                <Route path = "/contacts" component = {ListContactComponent}></Route>
+                <ListContactComponent />
+              </Switch>
+            </div>
+            
+            <Footer />
+        </Router>
       </div>
   );
 }
