a3ce18b7db973978157afae3586b5f7b002274a3
Renato Magrini <renatomagrini@gmail.com>
2021-09-13 22:30:57 +0200

    [3.2] Created get Contact by Id Rest API

--- contact-list-server/src/main/java/com/rvmagrini/contactlistserver/controller/ContactController.java
+++ contact-list-server/src/main/java/com/rvmagrini/contactlistserver/controller/ContactController.java
@@ -3,13 +3,16 @@ package com.rvmagrini.contactlistserver.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rvmagrini.contactlistserver.exception.ResourceNotFoundException;
 import com.rvmagrini.contactlistserver.model.Contact;
 import com.rvmagrini.contactlistserver.repository.ContactRepository;
 
@@ -34,6 +37,15 @@ public class ContactController {
 	public Contact addContact(@RequestBody Contact contact) {
 		return contactRepository.save(contact);
 	}
+	
+	// Get Contact by ID Rest API
+	@GetMapping("/contacts/{id}")
+	public ResponseEntity<Contact> getContactById(@PathVariable Long id) {
+		Contact contact = contactRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Contact with id " + id + "does not exist."));
+		return ResponseEntity.ok(contact);
+	}
+	
 	
 	
 
