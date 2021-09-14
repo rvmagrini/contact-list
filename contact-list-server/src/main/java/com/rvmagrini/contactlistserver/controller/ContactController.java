5bca7cea24e5d0250d27b0b3adfc8922e3be78aa
Renato Magrini <renatomagrini@gmail.com>
2021-09-14 09:56:37 +0200

    [4.1] Created deleteContact REST API

--- contact-list-server/src/main/java/com/rvmagrini/contactlistserver/controller/ContactController.java
+++ contact-list-server/src/main/java/com/rvmagrini/contactlistserver/controller/ContactController.java
@@ -1,10 +1,13 @@
 package com.rvmagrini.contactlistserver.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,23 +25,22 @@ import com.rvmagrini.contactlistserver.repository.ContactRepository;
 @RestController
 @RequestMapping("/api/v1/")
 public class ContactController {
-	
+
 	@Autowired
 	private ContactRepository contactRepository;
-	
+
 	// Get all contacts
 	@GetMapping("/contacts")
 	public List<Contact> getAllContacts() {
 		return contactRepository.findAll();
 	}
-	
-	
+
 	// Create Contact REST API
 	@PostMapping("/contacts")
 	public Contact addContact(@RequestBody Contact contact) {
 		return contactRepository.save(contact);
 	}
-	
+
 	// Get Contact by ID Rest API
 	@GetMapping("/contacts/{id}")
 	public ResponseEntity<Contact> getContactById(@PathVariable Long id) {
@@ -46,7 +48,7 @@ public class ContactController {
 				.orElseThrow(() -> new ResourceNotFoundException("Contact with id " + id + "does not exist."));
 		return ResponseEntity.ok(contact);
 	}
-	
+
 	// Update Contact REST API
 	@PutMapping("/contacts/{id}")
 	public ResponseEntity<Contact> updateContact(@PathVariable Long id, @RequestBody Contact contactDetails) {
@@ -55,12 +57,21 @@ public class ContactController {
 		contact.setFirstName(contactDetails.getFirstName());
 		contact.setLastName(contactDetails.getLastName());
 		contact.setEmail(contactDetails.getEmail());
-		
+
 		Contact updateContact = contactRepository.save(contact);
 		return ResponseEntity.ok(updateContact);
 	}
-	
-	
-	
+
+	// Delete Contact REST API
+	@DeleteMapping("/contacts/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteContact(@PathVariable Long id) {
+		Contact contact = contactRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Contact with id " + id + "does not exist."));
+
+		contactRepository.delete(contact);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
 
 }
